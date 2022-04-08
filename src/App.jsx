d201802eaf9b3a56adaed07f126599c43339f11d
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2022-04-08 12:00:36 +0430

    Now using backspace or delete will delete a number from input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Display from "./Components/Display";
 import Numpad from "./Components/Numpad";
 import Operatorpad from "./Components/Operatorpad";
 
 function App() {
     const [args, setArgs] = useState({ x: "0", operator: "", answer: "" });
-
+    useEffect(() => {
+        const BackSpaceHandler = (e) => {
+            if (e.keyCode === 8 || e.keyCode === 46) {
+                setArgs((previousArgs) => ({
+                    ...previousArgs,
+                    x:
+                        previousArgs.x.length === 1
+                            ? "0"
+                            : previousArgs.x.slice(0, -1),
+                }));
+            }
+        };
+        window.addEventListener("keydown", BackSpaceHandler);
+        return () => window.removeEventListener("keydown", BackSpaceHandler);
+    });
     return (
         <>
             <Numpad setter={setArgs} />
