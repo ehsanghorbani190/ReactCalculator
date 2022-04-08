3ba209c01cd932a3e06b3bbaed857df9a1487c95
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2022-04-08 11:58:16 +0430

    Added Key Binding to Buttons

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 const Button = ({ value, clickHandler }) => {
+    useEffect(() => {
+        const keyHandler = (e) => {
+            e.preventDefault(); // "/" key will mess up with focus, so we have to disable its functionality first
+            if (e.key === `${value}`.charAt(0)) clickHandler();
+        };
+        window.addEventListener("keypress", keyHandler);
+        return () => window.removeEventListener("keypress", keyHandler);
+    });
     return (
         <button type="button" onClick={clickHandler}>
             {value}
